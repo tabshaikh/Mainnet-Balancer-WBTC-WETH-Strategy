736f8dc960a5c0cf57bff81397f8901e1b0335ce
Tabish Shaikh <tabish@archive.org>
2021-09-22 13:23:39 +0530

    feat: added custom strategy resolvers

--- config/StrategyResolver.py
+++ config/StrategyResolver.py
@@ -11,34 +11,39 @@ class StrategyResolver(StrategyCoreResolver):
         (Strategy Must Implement)
         """
         # E.G
-        # strategy = self.manager.strategy
-        # return {
-        #     "gauge": strategy.gauge(),
-        #     "mintr": strategy.mintr(),
-        # }
-
-        return {}
+        strategy = self.manager.strategy
+        return {
+            "vault": strategy.VAULT(),
+            "pool": strategy.want(),
+            "lpcomponent": strategy.lpComponent(),
+        }
 
     def hook_after_confirm_withdraw(self, before, after, params):
         """
         Specifies extra check for ordinary operation on withdrawal
         Use this to verify that balances in the get_strategy_destinations are properly set
         """
-        assert False
+        # After withdraw from strategy the balance of pool should decrease
+        assert after.get("strategy.balanceOfPool") < before.get(
+            "strategy.balanceOfPool"
+        )
 
     def hook_after_confirm_deposit(self, before, after, params):
         """
         Specifies extra check for ordinary operation on deposit
         Use this to verify that balances in the get_strategy_destinations are properly set
         """
-        assert False
+        assert True
 
     def hook_after_earn(self, before, after, params):
         """
         Specifies extra check for ordinary operation on earn
         Use this to verify that balances in the get_strategy_destinations are properly set
         """
-        assert False
+        # Earn deposits the amount from sett to Strategy therefore check balance of Pool
+        assert after.get("strategy.balanceOfPool") > before.get(
+            "strategy.balanceOfPool"
+        )
 
     def confirm_harvest(self, before, after, tx):
         """
@@ -52,17 +57,17 @@ class StrategyResolver(StrategyCoreResolver):
             "sett.pricePerFullShare"
         )
 
-        # # Strategist should earn if fee is enabled and value was generated
-        # if before.get("strategy.performanceFeeStrategist") > 0 and valueGained:
-        #     assert after.balances("want", "strategist") > before.balances(
-        #         "want", "strategist"
-        #     )
+        # Strategist should earn if fee is enabled and value was generated
+        if before.get("strategy.performanceFeeStrategist") > 0 and valueGained:
+            assert after.balances("want", "strategist") > before.balances(
+                "want", "strategist"
+            )
 
-        # # Strategist should earn if fee is enabled and value was generated
-        # if before.get("strategy.performanceFeeGovernance") > 0 and valueGained:
-        #     assert after.balances("want", "governanceRewards") > before.balances(
-        #         "want", "governanceRewards"
-        #     )
+        # Strategist should earn if fee is enabled and value was generated
+        if before.get("strategy.performanceFeeGovernance") > 0 and valueGained:
+            assert after.balances("want", "governanceRewards") > before.balances(
+                "want", "governanceRewards"
+            )
 
     def confirm_tend(self, before, after, tx):
         """
@@ -72,4 +77,11 @@ class StrategyResolver(StrategyCoreResolver):
 
         (Strategy Must Implement)
         """
-        assert True
+        if before.get("strategy.balanceOfWant") > 0:
+            assert after.get("strategy.balanceOfWant") == 0
+            assert after.get("strategy.isTendable") == False
+
+            # Amount deposited in pool must have increased
+            assert after.get("strategy.balanceOfPool") > before.get(
+                "strategy.balanceOfPool"
+            )
